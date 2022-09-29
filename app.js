ee5011d7c2787a38d05f8d78fc152ab1ec4506f8
Essien Emmanuel Samuel <essienemma300@gmail.com>
2022-09-29 09:57:10 +0100

    more server boilerplate setups

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const userRoute = require('./routes/user');
 
 const port = 3000;
 
 const app = express();
+
+app.use(userRoute);
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(port, () => {
     console.log(`server spinning at port ${port}`);
