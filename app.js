6a320165168c8cfa12adb3deaedee09667f99e61
Essien Emmanuel Samuel <essienemma300@gmail.com>
2022-12-10 10:05:19 +0100

    started working on the database-with-mongodb

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const path = require("path");
 const shopRoute = require("./routes/shop");
 const adminRoute = require("./routes/admin");
 const errorController = require("./controllers/error");
-const sequelize = require("./utils/database");
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
+// const sequelize = require("./utils/database");
+// const Product = require("./models/product");
+// const User = require("./models/user");
+// const Cart = require("./models/cart");
+// const CartItem = require("./models/cart-item");
+// const Order = require("./models/order");
+// const OrderItem = require("./models/order-item");
 
 const port = 3000;
 
@@ -43,43 +43,43 @@ app.use(async (req, res, next) => {
 app.use(shopRoute);
 app.use("/admin", adminRoute);
 
-User.hasMany(Product);
-Product.belongsTo(User, {
-    constraints: true,
-    onDelete: "CASCADE",
-});
+// User.hasMany(Product);
+// Product.belongsTo(User, {
+//     constraints: true,
+//     onDelete: "CASCADE",
+// });
 
-User.hasOne(Cart);
-Cart.belongsTo(User);
+// User.hasOne(Cart);
+// Cart.belongsTo(User);
 
-Product.belongsToMany(Cart, { through: CartItem });
-Cart.belongsToMany(Product, { through: CartItem });
+// Product.belongsToMany(Cart, { through: CartItem });
+// Cart.belongsToMany(Product, { through: CartItem });
 
-User.hasMany(Order);
-Order.belongsTo(User);
+// User.hasMany(Order);
+// Order.belongsTo(User);
 
-Product.belongsToMany(Order, { through: OrderItem });
-Order.belongsToMany(Product, { through: OrderItem });
+// Product.belongsToMany(Order, { through: OrderItem });
+// Order.belongsToMany(Product, { through: OrderItem });
 
-const createTable = async () => {
-    try {
-        // await sequelize.sync({ force: true });
-        await sequelize.sync();
-        const user = await User.findOne({ where: { id: 1 } });
-        if (user) {
-            console.log("Found User");
-        }
-        await User.create({
-            name: "Max",
-            email: "max@gmail.com",
-        });
-        console.log("Tables created");
-        return await user.createCart();
-    } catch (error) {
-        console.log("error", error);
-        return;
-    }
-};
+// const createTable = async () => {
+//     try {
+//         // await sequelize.sync({ force: true });
+//         await sequelize.sync();
+//         const user = await User.findOne({ where: { id: 1 } });
+//         if (user) {
+//             console.log("Found User");
+//         }
+//         await User.create({
+//             name: "Max",
+//             email: "max@gmail.com",
+//         });
+//         console.log("Tables created");
+//         return await user.createCart();
+//     } catch (error) {
+//         console.log("error", error);
+//         return;
+//     }
+// };
 // createTable();
 
 //catch all errors for all routes and send the error page
